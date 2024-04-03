53b5b3dca91a5c3687ea2c4e59dd448b22e2068e
Ankur Laroia <49339040+ankurlaroia@users.noreply.github.com>
2024-04-02 20:08:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -19,12 +19,15 @@ app.post('/send-email', (req, res) => {
     const email = req.body.email;
     const message = req.body.message;
 
+    // Create a string containing name, email, and message
+    const emailContent = `Name: ${name}\nEmail: ${email}\n\nMessage: ${message}`;
+
     // Create a Nodemailer transporter using your email provider's SMTP settings
     const transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
-            user: 'ankurlaroia1@gmail.com', // Your Gmail address
-            pass: EMAIL_PASSWORD // Your Gmail password
+            user: 'ankurlaroia1@gmail.com', // Your email address
+            pass: process.env.EMAIL_PASSWORD // Use environment variable for email password
         }
     });
 
@@ -33,7 +36,7 @@ app.post('/send-email', (req, res) => {
         from: email, // Sender's email address
         to: 'ankurlaroia1@gmail.com', // Receiver's email address
         subject: 'New message from portfolio website',
-        text: `Name: ${name}\nEmail: ${email}\n\nMessage: ${message}` // Email content
+        text: emailContent // Use the created string as the email body
     };
 
     // Send the email
